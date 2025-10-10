0171e023e61d7c9be63e7ab38b5c4d407a719477
georgeweigt <55809489+georgeweigt@users.noreply.github.com>
2025-10-10 16:41:20 -0700

    Update paranoia.c

--- fp/paranoia.c
+++ fp/paranoia.c
@@ -2,8 +2,6 @@
 #include <stdlib.h>
 #include <math.h>
 
-void print4(float f);
-
 int
 main(void)
 {
@@ -16,6 +14,7 @@ main(void)
 	float F2;
 	float F3;
 	float F6;
+	float F9;
 	float O;
 	float O1;
 	float O2;
@@ -39,15 +38,15 @@ main(void)
 	O4 = 4.0;
 	T2 = 32.0;
 
-	// 1160 PRINT "Searching for radix  B  and precision  P ; ";
+// 1160 PRINT "Searching for radix  B  and precision  P ; ";
 
 	printf("Searching for radix B and precision P ; ");
 
-	// 1170 W=O1
+// 1170 W=O1
 
 	W = O1;
 
-	// 1180 W=W+W : Y=W+O1 : Z=Y-W : Y=Z-O1 : IF (F1+ABS(Y)<O) THEN 1180
+// 1180 W=W+W : Y=W+O1 : Z=Y-W : Y=Z-O1 : IF (F1+ABS(Y)<O) THEN 1180
 
 	do {
 		W = W + W;
@@ -56,12 +55,12 @@ main(void)
 		Y = Z - O1;
 	} while (F1 + fabsf(Y) < O);
 
-	// 1200 P=O :  Y=O1
+// 1200 P=O :  Y=O1
 
 	P = O;
 	Y = O1;
 
-	// 1210 B=W+Y : Y=Y+Y : B=B-W : IF (B=O) THEN 1210
+// 1210 B=W+Y : Y=Y+Y : B=B-W : IF (B=O) THEN 1210
 
 	do {
 		B = W + Y;
@@ -69,23 +68,22 @@ main(void)
 		B = B - W;
 	} while (B == O);
 
-	// 1220 IF (B<O2) THEN B=O1
+// 1220 IF (B<O2) THEN B=O1
 
 	if (B < O2)
 		B = O1;
 
-	// 1230 PRINT " Radix  B = "; B : IF (B=O1) THEN 1270
+// 1230 PRINT " Radix  B = "; B : IF (B=O1) THEN 1270
 
 	printf("Radix B = %g\n", B);
-
 	if (B == O1)
 		goto L1270;
 
-	// 1240 W=O1
+// 1240 W=O1
 
 	W = O1;
 
-	// 1250 P=P+O1 : W=W*B : Y=W+O1 : Z=Y-W : IF (Z=O1) THEN 1250
+// 1250 P=P+O1 : W=W*B : Y=W+O1 : Z=Y-W : IF (Z=O1) THEN 1250
 
 	do {
 		P = P + O1;
@@ -94,24 +92,24 @@ main(void)
 		Z = Y - W;
 	} while (Z == O1);
 
-	// 1270 U1=O1/W : U2=B*U1 : PRINT "Closest relative separation found is  U1 ="; U1
+// 1270 U1=O1/W : U2=B*U1 : PRINT "Closest relative separation found is  U1 ="; U1
 
 L1270:	U1 = O1 / W;
 	U2 = B * U1;
 	printf("Closest relative separation found is U1 = %g\n", U1);
 
-	// 1280 PRINT : PRINT "Recalculating radix and precision ";
+// 1280 PRINT : PRINT "Recalculating radix and precision ";
 
 	printf("Recalculating radix and precision ");
 
-	// 1290 E0=B : E1=U1 : E9=U2 : E3=P
+// 1290 E0=B : E1=U1 : E9=U2 : E3=P
 
 	E0 = B;
 	E1 = U1;
 	E9 = U2;
 	E3 = P;
 
-	// 1300 X=O4/O3 : F3=X-O1 : F6=F2-F3 : X=F6+F6 : X=ABS(X-F3) : IF (X<U2) THEN X=U2
+// 1300 X=O4/O3 : F3=X-O1 : F6=F2-F3 : X=F6+F6 : X=ABS(X-F3) : IF (X<U2) THEN X=U2
 
 	X = O4 / O3;
 	F3 = X - O1;
@@ -121,7 +119,7 @@ L1270:	U1 = O1 / W;
 	if (X < U2)
 		X = U2;
 
-	// 1320 U2=X : Y=F2*U2+T2*U2*U2 : Y=O1+Y : X=Y-O1 : IF (U2>X AND X>O) THEN 1320
+// 1320 U2=X : Y=F2*U2+T2*U2*U2 : Y=O1+Y : X=Y-O1 : IF (U2>X AND X>O) THEN 1320
 
 	do {
 		U2 = X;
@@ -130,7 +128,7 @@ L1270:	U1 = O1 / W;
 		X = Y - O1;
 	} while (U2 > X && X > O);
 
-	// 1340 X=O2/O3 : F6=X-F2 : F3=F6+F6 : X=F3-F2 : X=ABS(X+F6) : IF (X<U1) THEN X=U1
+// 1340 X=O2/O3 : F6=X-F2 : F3=F6+F6 : X=F3-F2 : X=ABS(X+F6) : IF (X<U1) THEN X=U1
 
 	X = O2 / O3;
 	F6 = X - F2;
@@ -140,7 +138,7 @@ L1270:	U1 = O1 / W;
 	if (X < U1)
 		X = U1;
 
-	// 1360 U1=X : Y=F2*U1+T2*U1*U1 : Y=F2-Y : X=F2+Y : Y=F2-X : X=F2+Y : IF (U1>X AND X>O) THEN 1360
+// 1360 U1=X : Y=F2*U1+T2*U1*U1 : Y=F2-Y : X=F2+Y : Y=F2-X : X=F2+Y : IF (U1>X AND X>O) THEN 1360
 
 	do {
 		U1 = X;
@@ -151,23 +149,24 @@ L1270:	U1 = O1 / W;
 		X = F2 + Y;
 	} while (U1 > X && X > O);
 
-	// 1380 IF (U1=E1) THEN PRINT " confirms closest relative separation  U1 ."
+// 1380 IF (U1=E1) THEN PRINT " confirms closest relative separation  U1 ."
 
 	if (U1 == E1)
 		printf("confirms closest relative separation U1.\n");
 
-	// 1390 IF (U1><E1) THEN PRINT " gets better closest relative separation  U1 = "; U1
+// 1390 IF (U1><E1) THEN PRINT " gets better closest relative separation  U1 = "; U1
 
 	if (U1 != E1)
 		printf("gets better closest relative separation U1 = %g\n", U1);
-}
 
-void
-print4(float f)
-{
-	printf("\n%g %02x %02x %02x %02x\n", f,
-		((unsigned char *) &f)[3],
-		((unsigned char *) &f)[2],
-		((unsigned char *) &f)[1],
-		((unsigned char *) &f)[0]);
+// 1400 W=O1/U1 : F9=(F2-U1)+F2
+
+	W = O1 / U1;
+	F9 = (F2 - U1) + F2;
+
+// 1410 B=INT(.01 + U2/U1) : IF (B=E0) THEN PRINT "Radix  B  confirmed."
+
+	B = floorf(0.01 + U2 / U1);
+	if (B == E0)
+		printf("Radix B confirmed.\n");
 }
